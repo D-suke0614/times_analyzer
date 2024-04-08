35630d61e487ef1e1da8fea74d91d492d0c89993
Daisuke <daisuke.kida@nijibox.co.jp>
2024-04-08 18:48:35 +0900

    fix: 見た目の修正

--- src/app/analyze/[id]/loading.tsx
+++ src/app/analyze/[id]/loading.tsx
@@ -2,7 +2,7 @@ const Loading = () => {
   return (
     <div className='flex justify-center items-center gap-6 mt-10'>
       <div className='h-10 w-10 animate-spin border-[5px] border-sky-400 rounded-full  border-t-transparent'></div>
-      <p className='text-[30px] font-weight'>Analyzing</p>
+      <p className='text-[30px] font-weight'>Analyzing...</p>
     </div>
   )
 }
