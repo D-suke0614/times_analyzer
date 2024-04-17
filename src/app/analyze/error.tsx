5f6ae3214510f61c7b273ebeebe1876dceca58ec
Daisuke <daisuke.kida@nijibox.co.jp>
2024-04-17 13:59:45 +0900

    format

--- src/app/analyze/error.tsx
+++ src/app/analyze/error.tsx
@@ -3,12 +3,14 @@
 import Link from 'next/link'
 import React from 'react'
 
-function Error({error}: {error: Error}) {
+function Error({ error }: { error: Error }) {
   console.log(error)
   return (
     <div className='flex flex-col items-center gap-7 mt-10'>
       <h2 className=' font-bold text-xl'>An error has occurred.</h2>
-      <Link href={'/'} className='hover:underline'>トップページへ戻る</Link>
+      <Link href={'/'} className='hover:underline'>
+        トップページへ戻る
+      </Link>
     </div>
   )
 }
