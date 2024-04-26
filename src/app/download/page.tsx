2d93f0a57c72cb0690954483f268911bf134ea97
Daisuke <daisuke.kida@nijibox.co.jp>
2024-04-26 17:04:46 +0900

    チャンネル情報取得の動線整備

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -26,8 +26,8 @@ function Page() {
             Slackからtimesチャンネルの情報を取得します。<br/>
             この処理には時間がかかることがあります。
           </p>
-          <div className='flex justify-start gap-8 mt-5'>
-            <Link className='py-2 px-5 bg-gray-400 rounded-md hover:opacity-75' href={'/'}>戻る</Link>
+          <div className='flex justify-start gap-7 mt-5'>
+            <Link className='py-2 px-5 bg-gray-400 rounded-md hover:opacity-75 hover:cursor-pointer' href={'/'}>戻る</Link>
             <button className='py-2 px-5 bg-cyan-500 rounded-md hover:opacity-75' type='button' onClick={fetchChannelList}>
               開始
             </button>
