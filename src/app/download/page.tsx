f608e893858b34f8b278b8fd8971765178fceaf2
Daisuke <daisuke.kida@nijibox.co.jp>
2024-04-26 12:01:03 +0900

    feat: チャンネル情報取得の動線を整備

--- src/app/download/page.tsx
+++ src/app/download/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import React, { useState } from 'react'
 
@@ -14,16 +15,24 @@ function Page() {
   }
   return (
     <div>
-      <button type='button' onClick={fetchChannelList}>
-        チャンネル情報を取得する
-      </button>
       {isDownloading ? (
         <div className='flex justify-center items-center gap-6 mt-20'>
           <div className='h-10 w-10 animate-spin border-[5px] border-sky-400 rounded-full  border-t-transparent'></div>
           <p className='text-[30px] font-weight'>Downloading...</p>
         </div>
       ) : (
-        <></>
+        <div className='mx-auto mt-20 w-fit'>
+          <p>
+            Slackからtimesチャンネルの情報を取得します。<br/>
+            この処理には時間がかかることがあります。
+          </p>
+          <div className='flex justify-start gap-8 mt-5'>
+            <Link className='py-2 px-5 bg-gray-400 rounded-md hover:opacity-75' href={'/'}>戻る</Link>
+            <button className='py-2 px-5 bg-cyan-500 rounded-md hover:opacity-75' type='button' onClick={fetchChannelList}>
+              開始
+            </button>
+          </div>
+        </div>
       )}
     </div>
   )
