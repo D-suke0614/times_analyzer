f3257a0c0daa09c38e885ea8fe5554bba1bda15e
Daisuke <daisuke.kida@nijibox.co.jp>
2024-05-07 14:53:28 +0900

    feat: チャンネルが選択されていない状態で、分析画面を開いた時の挙動

--- src/app/analyze/error.tsx
+++ src/app/analyze/error.tsx
@@ -7,7 +7,7 @@ function Error({ error }: { error: Error }) {
   console.log(error)
   return (
     <div className='flex flex-col items-center gap-7 mt-10'>
-      <h2 className=' font-bold text-xl'>An error has occurred.</h2>
+      <h2 className='font-bold text-xl'>An error has occurred.</h2>
       <Link href={'/'} className='hover:underline'>
         トップページへ戻る
       </Link>
